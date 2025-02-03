55df3af8f17b2b06008e4aa262f7a1f9c303b306
Liam Henry <Liam.Henry@umuzi.org>
2025-02-03 15:50:34 +0200

    fixed blcoked location response

--- src/script.js
+++ src/script.js
@@ -2,22 +2,25 @@ const { default: axios } = require("axios");
 (async () => {
 
     const weather = document.getElementById("weather");
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(async (pos) => {
-            const weatherURL = `https://wttr.in/${pos.coords.latitude},${pos.coords.longitude}?format=2`;
-            const locationURL = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}`;
 
-            const fetchWeather = async () => await axios.get(weatherURL);
-            const fetchReadableLocation = async () => await axios.get(locationURL);
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+        const weatherURL = `https://wttr.in/${pos.coords.latitude},${pos.coords.longitude}?format=2`;
+        const locationURL = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}`;
 
-            const { data } = await fetchWeather();
-            const location = await fetchReadableLocation().then(res => res.data)
-            weather.innerHTML = `${location.address.country}, ${location.address.county}: 
+        const fetchWeather = async () => await axios.get(weatherURL);
+        const fetchReadableLocation = async () => await axios.get(locationURL);
+
+        const { data } = await fetchWeather();
+        const location = await fetchReadableLocation().then(res => res.data)
+        weather.innerHTML = `${location.address.country}, ${location.address.county}: 
             ${data}`
-        });
-    } else {
-        weather.innerHTML = "Could not verify geolocation";
-    }
+    });
+
+    await navigator.permissions.query({ name: 'geolocation' }).then(permissions => {
+        if (permissions.state === "denied") {
+            weather.innerHTML = `Location permissions not granted`
+        }
+    })
 })()
 
 
