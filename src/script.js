487e677dc5cb7ee6289f95e6cb5058ab17598450
Liam Henry <Liam.Henry@umuzi.org>
2025-02-03 14:39:26 +0200

    fixing location bug

--- src/script.js
+++ src/script.js
@@ -1,10 +1,27 @@
 const { default: axios } = require("axios");
 (async () => {
-    const weather = document.getElementById("weather");
-    const weatherURL = `https://wttr.in/?format=3&geoip=true`;
-    const fetchWeather = async () =>
-        await axios.get(weatherURL);
 
-    const { data } = await fetchWeather();
-    weather.innerHTML = data
+    const weather = document.getElementById("weather");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (pos) => {
+            const weatherURL = `https://wttr.in/${pos.coords.latitude},${pos.coords.longitude}?format=3`;
+            const locationURL = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}`;
+
+            const fetchWeather = async () =>
+                await axios.get(weatherURL);
+            const fetchReadableLocation = async (lat, long) => await axios.get(locationURL);
+
+            const { data } = await fetchWeather();
+            const location = await fetchReadableLocation(pos.coords.latitude, pos.coords.longitude).then(res => res.data)
+
+            weather.innerHTML = `${location.address.country} ${location.address.county} 
+            ${data.split(":")[1]}`
+        });
+    } else {
+        weather.innerHTML = "Geolocation is not supported by this browser.";
+    }
 })()
+
+
+
+
